d4435bd3f7879c64b91ac9bab7bebe9475922b98
Giacomo <giacomo.matteucci@studenti.unicam.it>
2021-12-22 14:58:40 +0100

    First Commit

---
+++ src/main/java/it/unicam/cs/ids/Casotto/CasottoApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.Casotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/ids/Casotto/CasottoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.Casotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
