0512f52fac0e586e122d56af9ebb4c8e00c5368c
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-01-24 23:28:29 +0100

    Eliminazione import inutilizzati

--- src/main/java/it/unicam/cs/ids/Casotto/Repository/PrenotazioniRepository.java
+++ src/main/java/it/unicam/cs/ids/Casotto/Repository/PrenotazioniRepository.java
@@ -3,12 +3,8 @@ package it.unicam.cs.ids.Casotto.Repository;
 import it.unicam.cs.ids.Casotto.Classi.Account;
 import it.unicam.cs.ids.Casotto.Classi.Ombrellone;
 import it.unicam.cs.ids.Casotto.Classi.Prenotazione;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.Entity;
 import java.time.LocalDate;
