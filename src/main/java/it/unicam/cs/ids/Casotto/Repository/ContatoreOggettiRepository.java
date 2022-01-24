ccf7114d140ccd1cfc41a8209be7c90b7da3f691
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-01-24 23:25:38 +0100

    Ricaricamento ContatoreOggettiRepository

--- src/main/java/it/unicam/cs/ids/Casotto/Repository/ContatoreOggettiRepository.java
+++ src/main/java/it/unicam/cs/ids/Casotto/Repository/ContatoreOggettiRepository.java
@@ -1,14 +1,20 @@
 package it.unicam.cs.ids.Casotto.Repository;
 
 import it.unicam.cs.ids.Casotto.Classi.ContatoreOggetti;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.Entity;
 
 /**
  * {@link Repository} per l'{@link Entity} {@link ContatoreOggetti}
  *
  */
+@Transactional
 public interface ContatoreOggettiRepository extends CrudRepository<ContatoreOggetti, Long> {
 
     /**
