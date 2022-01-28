8ce4fc09e766c0d64f2c16d20f5eb285a391d022
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-01-28 16:49:22 +0100

    Eliminazione metodo findById

--- src/main/java/it/unicam/cs/ids/Casotto/Repository/OrdinazioneRepository.java
+++ src/main/java/it/unicam/cs/ids/Casotto/Repository/OrdinazioneRepository.java
@@ -5,15 +5,14 @@ import it.unicam.cs.ids.Casotto.Classi.Stato;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Stack;
 
 @Repository
 public interface OrdinazioneRepository extends CrudRepository<Ordinazione, Long> {
 
-    Ordinazione findById(long id);
-
     boolean existsById(long id);
 
-    List<Ordinazione> findByStato(Stato stato);
+    List<Ordinazione> findByStatoAndData(Stato stato, LocalDate data);
 }
