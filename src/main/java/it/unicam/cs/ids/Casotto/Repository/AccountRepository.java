82455fd5ebeb6603c2db26a2cd2bb914cf32edf8
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-01-28 16:41:12 +0100

    Implementazione query per l'aggiornamento di saldo, email e password di un account

--- src/main/java/it/unicam/cs/ids/Casotto/Repository/AccountRepository.java
+++ src/main/java/it/unicam/cs/ids/Casotto/Repository/AccountRepository.java
@@ -2,8 +2,11 @@ package it.unicam.cs.ids.Casotto.Repository;
 
 import it.unicam.cs.ids.Casotto.Classi.Account;
 import it.unicam.cs.ids.Casotto.Classi.Utente;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
@@ -19,5 +22,23 @@ public interface AccountRepository extends CrudRepository<Account, Long> {
     boolean existsByEmailIgnoreCase(String email);
 
     Account findByIscrizioniId(long id);
+
+    Account findByPrenotazioniId(long id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Account a SET a.saldo = ?2 WHERE a.id = ?1")
+    void updateAccountSaldoById(long id, double saldo);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Account a SET a.email = ?2 WHERE a.id = ?1")
+    void updateAccountEmailById(long id, String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Account a SET a.password = ?2 WHERE a.id = ?1")
+    void updateAccountPasswordById(long id, int psw);
+
 
 }
